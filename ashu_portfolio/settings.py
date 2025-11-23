2ee8549d4925e83826553b82e4861d18889a41b8
Ashutosh Singh <ashu210103@gmail.com>
2025-11-23 12:52:16 +0530

    Added gunicorn to requirements

--- ashu_portfolio/settings.py
+++ ashu_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kj!gu28g*h)o2+1qp!wcts$w!icxh%1x!!h7=!-0%#wgw!@i%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
